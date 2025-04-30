dd4fe5c27caddcab5cbb6500f428080426493d98
jaesu ryu <jaesu@kakao.com>
2025-04-30 09:45:03 +0900

    fix: 존재하지 않는 custom.css 참조 제거

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="description" content="경제제재 정보 검색 시스템" />
         <meta name="theme-color" content="#3B82F6" />
-        <link rel="stylesheet" href="/css/custom.css" />
+        <link rel="stylesheet" href="/css/style.css" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
